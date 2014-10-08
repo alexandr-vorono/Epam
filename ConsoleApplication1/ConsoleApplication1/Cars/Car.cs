418cf4908e2b316325b966906812d0604d7e282d
Epam lab1 <sanya_1_9_9_2@mail.ru>
2014-10-08 12:00:03 +0300

    Реализованно добавление в файл

---
+++ ConsoleApplication1/ConsoleApplication1/Cars/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Car
+    {
+        public string name {get; set;}
+        public int model {get; set;}
+        public int speed {get; set;}
+      
+        public Car() { }
+        public Car(string name, int model, int speed)
+        {
+            this.name = name;
+            this.model = model;
+            this.speed = speed;
+            Console.WriteLine("sfgsdfgsdfgsdfgsdfg");
+        }
+       
+       protected String Info()
+        {
+            return "\n\n\n bvz" + this.name.ToString();
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Taxi.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Taxi
+    {
+       
+        private List<Car> list = new List<Car>();
+
+        public List<Car> CarsList
+        {
+            get { return list; }
+            set { list = value; }
+        }
+        public override String ToString()
+        {
+            string s = "";
+            foreach (var t in list)
+                s += "\n" + t.name + "\n" + t.model;
+            return s;
+        }
+     }
+}
