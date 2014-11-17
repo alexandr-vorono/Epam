311133a185c9c4c78538decbb89db1d761cf058d
Epam lab1 <sanya_1_9_9_2@mail.ru>
2014-11-17 14:35:14 +0300

    Добавлены методы подключения\отключения терминала от  порта

--- ATS/ATS/Terminal.cs
+++ ATS/ATS/Terminal.cs
@@ -9,22 +9,33 @@ namespace ATS
 {
     public class Terminal
     {
+        //Номер вызываемого
         private int opponentNumber;
+        //Порт
         public Port Port { get; set; }
+        //Является ли телеон  вызывающим
         private bool isCallInitiator = false;
+        /// <summary>
+        /// Конструктор с параметрами
+        /// </summary>
+        /// <param name="port">Порт</param>
         public Terminal(Port port)
         {
             this.Port = port;
         }
+        //Соытие исходящего  вызова
         public delegate void ToCall(int output, int input);
         public event ToCall CallToEven;
-
+        //Событие ответа на звонок
         public delegate void AnswerCallDelegate(int output, int input);
         public event AnswerCallDelegate AnswerCallToEvent;
-
+        //Событие завершения звонка
         public delegate void EndCallDelegate(int output, int input);
         public event EndCallDelegate EndCallToEvent;
-
+        /// <summary>
+        /// Метод совершения звонка
+        /// </summary>
+        /// <param name="receiveNumber">Номер телефона вызываемого</param>
         public void CallTo(int receiveNumber)
         {
             if (Port.PhoneNumber != receiveNumber)
@@ -47,7 +58,11 @@ namespace ATS
             }
             else Console.WriteLine("Себе звонить нельзя.");
         }
-
+        /// <summary>
+        /// Метод вызывающий событие исходящего  звонка
+        /// </summary>
+        /// <param name="output">Номер вызывающего</param>
+        /// <param name="input">Номер вызываемого</param>
         public virtual void OnCallToEvent(int output, int input)
         {
             if (CallToEven != null)
@@ -55,14 +70,19 @@ namespace ATS
                 CallToEven(output, input);
             }
         }
-
+        /// <summary>
+        /// Метод вызывающий событие входящего звонка
+        /// </summary>
+        /// <param name="incomingNumber">Номер вызываемого</param>
         public void IncomingCall(int incomingNumber)
         {
             Port.State = PortState.InputCall;
             this.opponentNumber = incomingNumber;
             isCallInitiator = false;
         }
-
+        /// <summary>
+        /// Метод вызываемый при ответе на звонок
+        /// </summary>
         public void AnswerCall()
         {
             if (Port.State == PortState.InputCall)
@@ -72,7 +92,11 @@ namespace ATS
                 Console.WriteLine("Абонент  {0}  ответил ", Port.PhoneNumber);
             }
         }
-
+        /// <summary>
+        /// Метод вызывающий событие ответа на звонок
+        /// </summary>
+        /// <param name="output">Номер вызывающего</param>
+        /// <param name="input">Номер вызываемого</param>
         public virtual void OnAnswerCallToEvent(int output, int input)
         {
             if (AnswerCallToEvent != null)
@@ -80,7 +104,9 @@ namespace ATS
                 AnswerCallToEvent(output, input);
             }
         }
-
+        /// <summary>
+        /// Метод завершает звонок
+        /// </summary>
         public void EndCall()
         {
             if (Port.State == PortState.Busy)
@@ -93,6 +119,11 @@ namespace ATS
                 Console.WriteLine("Звонок между {0} и {1} завершен", Port.PhoneNumber, opponentNumber);
             }
         }
+        /// <summary>
+        /// Метод вызывающий событие завершения звока
+        /// </summary>
+        /// <param name="output">Номер вызывающего</param>
+        /// <param name="input">Номер вызываемого</param>
         public virtual void OnEndCallToEvent(int output, int input)
         {
             if (EndCallToEvent != null)
@@ -100,10 +131,29 @@ namespace ATS
                 EndCallToEvent(output, input);
             }
         }
-
+        /// <summary>
+        /// Метод вызываемый при завершении звонка
+        /// </summary>
         public void AbonentEndCall()
         {
             Port.State = PortState.Connected;
+        }
+        /// <summary>
+        /// Метод подключения телефона к порту.
+        /// </summary>
+        public void Connect()
+        {
+            if (Port.State == PortState.Disconnected)
+                Port.State = PortState.Connected;
+        }
+
+        /// <summary>
+        /// Метод отключения телефона от порта.
+        /// </summary>
+        public void Disconnect()
+        {
+            if (Port.State == PortState.Connected)
+                Port.State = PortState.Disconnected;
         }
     }
 }
--- ATS/ATS/Timer.cs
+++ ATS/ATS/Timer.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace ATS
 {
+    //Класс реализует  ускоренное протекание времени
     public class Timer
     {
         private DateTime time;
